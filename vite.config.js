6da27a5d6e274c7c8583ac92077b0af846624897
Mustafa Alkan <mustafaalkanx@gmail.com>
2026-03-03 10:01:24 +0300

    Fix: formm ayarları

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        tesekkurler: resolve(__dirname, 'tesekkurler.html'),
         success: resolve(__dirname, 'success.html'),
       },
     },
